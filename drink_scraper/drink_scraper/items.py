345652adf704e5504c2b51ac81ea6ffda0eaa718
Devin Bhushan <devinbhushan@gmail.com>
2013-04-18 00:28:53 -0500

    Added Drink item to items.py and example documentation explaining the standard

--- drink_scraper/drink_scraper/items.py
+++ drink_scraper/drink_scraper/items.py
@@ -5,7 +5,26 @@
 
 from scrapy.item import Item, Field
 
-class DrinkScraperItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+
+class Drink(Item):
+    '''
+    name = String
+    ingredients = [(quantity, name, unit), ..., (quantity_last, name_last, unit_last)]
+    rating = Integer
+    num_reviews = Integer
+    directions = [String, String, String]
+    tags = [String, String, String]
+    Example:
+        name = "Admiral"
+        ingredients = [(1, bourbon, oz), (1, dry vermouth, oz), ...]
+        rating = 4.67
+        num_reviews = 3
+        directions = ["Step1", "Step2", "Step3"]
+        tags = ["bourbon", "dry_vermouth", "lemon", "lemon juice", ...]
+    '''
+    name = Field()
+    ingredients = Field()
+    num_reviews = Field()
+    directions = Field()
+    rating = Field()
+    tags = Field()
