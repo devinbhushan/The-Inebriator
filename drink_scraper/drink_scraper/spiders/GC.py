34aebb52a8935fe6c8335bf11a580eca304d6224
Jeff Bailey <jbailey2010@gmail.com>
2013-04-18 23:26:09 -0500

    Good Cocktails parse is done
    Still need to fix up parseDrink

---
+++ drink_scraper/drink_scraper/spiders/GC.py
@@ -0,0 +1,49 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from drink_scraper.items import Drink
+from scrapy import log
+from scrapy.http import Request
+
+
+class Dofw(BaseSpider):
+    """
+    Spider for Good Cocktails
+    """
+    name = "gc"
+    allowed_domains = ["goodcocktails.com"]
+    start_urls = ["http://www.goodcocktails.com/recipes/browse_drinks.php?letter=ALL"]
+
+    def parse(self, response):
+        """
+        Parse function to get each drink's link and parse it
+        """
+        log.msg("Beginning parse function", level=log.INFO)
+        hxs = HtmlXPathSelector(response)
+        outer_links = hxs.select("//div[@id='drinkList']/ul/li")
+        log.msg('number outer_links: %s' % len(outer_links), level=log.INFO)
+
+        for outer_link in outer_links:
+            links = outer_links.select('a/@href').extract()
+            for link in links:
+                #log.msg('following %s' % link, level=log.INFO)
+                yield Request(link, callback=self.parseDrink)
+
+    def parseDrink(self, response):
+        """
+        Parse function to go grab Drink info from each drink
+        Returns Drink Item
+        """
+        hxs = HtmlXPathSelector(response)
+        drink = Drink()
+
+        drink['name'] = hxs.select("//h2[@class='pagetitle']/text()")
+        drink['rating'] = hxs.select("//meta[@itemprop='ratingValue']/@content")
+        drink['num_reviews'] = hxs.select("//meta[@itemprop='ratingCount']/@content")
+
+        drink['tags'] = []
+        tags = hxs.select("//div[@class='posttags']/a[@rel='tag']")
+        for tag in tags:
+            drink['tags'].append(tag.select("text()"))
+
+        log.msg('Drink retrieved: %s' % drink, level=log.INFO)
+        return drink
