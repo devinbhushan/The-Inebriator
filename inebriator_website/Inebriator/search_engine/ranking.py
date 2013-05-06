8dcd3c4124bec794b3a4066585e136e3c815c7b2
michael.b.willingham <michael.b.willingham@gmail.com>
2013-05-06 01:56:26 -0500

    Made frequency.py run 20 minutes faster. Made frequency more realstici.... Now it breaks ingredients up into terms instead of taking the entire ingredient name as a term. Fixed ranking

---
+++ inebriator_website/Inebriator/search_engine/ranking.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import ast
+from math import log
+from Queue import PriorityQueue
+import operator
+
+PWD = os.path.dirname(os.path.realpath(__file__))
+filepath = os.path.abspath(os.path.join(PWD, "..", ".."))
+sys.path.append(filepath)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Inebriator.settings'
+
+from models import *
+
+def rank(query_terms, drinks):
+    avg_len = 0
+    counter = 0
+    sorted_drinks = {}
+
+    #Get the average length per document
+    for drink in drinks:
+        ingredients = drink.ingredients.all()
+        for ingredient in ingredients:
+            avg_len += 1.0
+        counter += 1.0
+    avg_len /= counter
+
+    #For each drink, get a score
+    for drink in drinks:
+        score = 0.0
+
+        #Sum up all the terms to get the score
+        for term in query_terms:
+            term_object = Frequency.objects.get(name=term.lower())
+            term_freq = 0.0
+            if term_object is not None:
+                term_freq = float(term_object.quantity)
+	        #idf below
+            idf = log((len(drinks)-term_freq+.5)/(term_freq+.5),2)
+            ingredients = drink.ingredients.all()
+            doc_freq = 0.0
+            total_freq = 0.0
+            for ingredient in ingredients:
+                if (term.lower() in ingredient.name.lower()):
+                    doc_freq += 1.0
+                total_freq += 1.0
+            #doc frequency below
+            doc_freq /= total_freq
+            #k, b below, |d| is total_freq
+            b = 0.75
+            k = 1.2
+            numerator = doc_freq*(k + 1)*idf
+            denominator = doc_freq + k*(1 - b + b * (total_freq/avg_len))
+            score += numerator / denominator
+        #print drink.name.encode('utf-8')
+        #print 100-score
+        sorted_drinks[drink] = 100-score
+    sorted_dict = sorted(sorted_drinks.iteritems(), key=operator.itemgetter(1))
+    #sorted_drinks = dict(sorted_dict)
+    for drink in sorted_dict:
+        print drink[0].name.encode('utf-8'), ":", drink[1]
+    return sorted_dict
